239aff99b9d08f9964d4005722e2a27b67cea4a8
gijsmolenaar <gijsmolenaar@3eaa9f62-2d11-11df-9942-15a8a56ed3ad>
2010-05-17 13:52:02 +0000

    made it working on my mac

--- trunk/sonic-gesture/src/qttest.cpp
+++ trunk/sonic-gesture/src/qttest.cpp
@@ -10,7 +10,7 @@ QtTestor::QtTestor(QWidget *parent) : QWidget(parent) {
     timer = new QTimer();
     timer = new QTimer(this);
     connect(timer,SIGNAL(timeout()),this,SLOT(queryFrame()));
-    timer->start(1);
+    timer->start(40);
 };
 
 QtTestor::~QtTestor() {};
@@ -21,12 +21,12 @@ void QtTestor::queryFrame() {
 }
 
 void QtTestor::paintEvent(QPaintEvent* e) {
-    painter = new QPainter(this);
+    QPainter painter(this);
     cvtColor(bgr, rgb, CV_BGR2RGB);
     const unsigned char * data = (unsigned char *)(rgb.data);
     //frame.step;
     qframe = QImage(data, rgb.cols, rgb.rows, QImage::Format_RGB888);
-    painter->drawImage(QPoint(0, 0),qframe);
+    painter.drawImage(QPoint(0, 0),qframe);
 };
 
 int main(int argc, char *argv[]) {

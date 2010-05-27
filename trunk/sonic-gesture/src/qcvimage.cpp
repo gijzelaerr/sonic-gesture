18f7e0cc71ef322867485dc4011860a8e649eca5
gijsmolenaar <gijsmolenaar@3eaa9f62-2d11-11df-9942-15a8a56ed3ad>
2010-05-27 15:17:50 +0000

    think stuff should work better now

--- trunk/sonic-gesture/src/qcvimage.cpp
+++ trunk/sonic-gesture/src/qcvimage.cpp
@@ -12,17 +12,15 @@ QCVImage::~QCVImage() {};
 
 void QCVImage::setImage(Mat* bgr) {
     this->bgr = bgr;
+    //this->resize(bgr->cols, bgr->rows);
     //this->setMinimumSize(bgr->cols, bgr->rows);
+    //this->setMaximumSize(bgr->cols, bgr->rows);
 }
 
 void QCVImage::paintEvent(QPaintEvent* e) {
     QPainter painter(this);
-    this->resize(bgr->cols, bgr->rows);
-
-    //this->setMaximumSize(bgr->cols, bgr->rows);
     cvtColor(*bgr, rgb, CV_BGR2RGB);
-    const unsigned char* data = (unsigned char*)(rgb.data);
-    qframe = QImage(data, rgb.cols, rgb.rows, QImage::Format_RGB888);
+    qframe = QImage((unsigned char*)(rgb.data), rgb.cols, rgb.rows, QImage::Format_RGB888);
     qframe = qframe.scaled(this->size(), Qt::KeepAspectRatio);
     painter.drawImage(QPoint(0, 0),qframe);
 };

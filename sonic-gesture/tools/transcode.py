c9b0f45cf7d6d5f52de799913c5d26d3cc8721cc
gijsmolenaar <gijsmolenaar@gmail.com>
2010-06-22 12:24:13 +0000

    fixed dataset settings, added multicore transcode script

---
+++ sonic-gesture/tools/transcode.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+cores = 4
+
+import sys
+import os
+import subprocess
+from multiprocessing import Pool
+
+
+def transcode((source, destination)):
+    cmd = ["/usr/bin/transcode", "-i", source, "-y" "ffmpeg", "-o", destination, "-F wmv2"]
+    return subprocess.call(cmd)
+
+def main():
+    if len(sys.argv) != 2:
+        print("usage: %s <dir with movies>" % sys.argv[0])
+        sys.exit(1)
+
+    moviedir=sys.argv[1]
+    movies = [x for x in os.listdir(moviedir) if x.endswith('avi')]
+
+    jobs = []
+    for movie in movies:
+        source = os.path.join(moviedir, movie)
+        name = movie.split('.')[:-1]
+        destionation = "small_" + movie
+        jobs.append((source, destionation))
+
+    pool = Pool(processes=cores)
+    vals = pool.map(transcode, jobs, chunksize=1)
+
+
+if __name__ == '__main__':
+    main()

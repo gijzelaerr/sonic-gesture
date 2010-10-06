a6e7a6c18686f02fab73e4a04ad5f51d6a88383a
gijsmolenaar@gmail.com <gijsmolenaar@gmail.com@3eaa9f62-2d11-11df-9942-15a8a56ed3ad>
2010-10-06 16:52:05 +0000

    added note

--- trunk/sonic-gesture/src/skinfinder.cpp
+++ trunk/sonic-gesture/src/skinfinder.cpp
@@ -63,11 +63,6 @@ void SkinFinder::make_histogram() {
     if (!(face == Rect())) {
         facepixels = hsv(face);
         histogram.update(facepixels);
-
-        //Mat gijs = frame(face);
-        //Mat gijs2;
-        //resize(gijs, gijs2, Size(), 5, 5);
-        //imshow("gijs", gijs2);
     }
 }
 
